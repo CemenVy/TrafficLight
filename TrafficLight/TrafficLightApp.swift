3ec671d283ecbfefb0caea30f8b6f054a883bd3e
Семен Выдрин <pw5hv7ngcv@privaterelay.appleid.com>
2023-10-12 18:52:01 +0400

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Семен Выдрин on 12.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
